08cd1ddfd50ada17c8ddf3999724a0fe6c034b36
Simran Shah <simransshah2@gmail.com>
2019-10-06 13:41:38 -0400

    Updated SQL schema

--- app/data/sql-schema.sql
+++ app/data/sql-schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE Person (
     gender VARCHAR(64) NOT NULL,
     startDate DATE NOT NULL,
     position VARCHAR(64),
-    radio VARCHAR (64),
+    radioNumber VARCHAR (64),
     isActive BOOLEAN
 );
 
@@ -17,23 +17,33 @@ CREATE TABLE PersonPhone(
   phoneType VARCHAR(64),
   phoneNumber VARCHAR (64),
   PRIMARY KEY (personId, phoneNumber),
-  FOREIGN KEY (personId) REFERENCES Person(personId)
+  FOREIGN KEY (personId) REFERENCES Person (personId)
 );
 
 CREATE TABLE Station(
   stationId INTEGER PRIMARY KEY AUTO_INCREMENT,
-  cheifId INTEGER,
-  FOREIGN KEY (cheifId) REFERENCES Person(personId)
+  chiefId INTEGER,
+  FOREIGN KEY (chiefId) REFERENCES Person (personId)
 );
 
 
 CREATE TABLE FireFighter (
-  personId INTEGER,
+  personId INTEGER PRIMARY KEY AUTO_INCREMENT,
+  firstName VARCHAR(64),
+  lastName VARCHAR(64),
+  address VARCHAR (64),
+  email VARCHAR(64),
+  dob DATE NOT NULL,
+  gender VARCHAR(64) NOT NULL,
+  startDate DATE NOT NULL,
+  position VARCHAR(64),
+  radioNumber VARCHAR (64),
+  isActive BOOLEAN,
   stationId INTEGER,
-  FOREIGN KEY (stationId) REFERENCES Station(stationId)
+  FOREIGN KEY (stationId) REFERENCES Station (stationId)
 );
 
-CREATE TABLE CERTS (
+CREATE TABLE Certificate (
   certId INTEGER PRIMARY KEY AUTO_INCREMENT
   certAgency VARCHAR(64),
   certName  VARCHAR(64),
@@ -46,16 +56,16 @@ CREATE TABLE CertTacking(
   dateRenewed DATE NOT NULL,
   expDate DATE,
   PRIMARY KEY (personId, certId),
-  FOREIGN KEY (personId) REFERENCES Person(personId)
-  FOREIGN KEY (certId) REFERENCES CERTS(certId)
+  FOREIGN KEY (personId) REFERENCES Person (personId)
+  FOREIGN KEY (certId) REFERENCES Certificate (certId)
 );
 
-CREATE TABLE ExpiredTracking(
+CREATE TABLE ExpiredCertTracking(
   personId INTEGER,
   certId INTEGER,
   dateRenewed DATE NOT NULL,
   expDate DATE,
   PRIMARY KEY (personId, certId),
-  FOREIGN KEY (personId) REFERENCES CertTacking(personId)
-  FOREIGN KEY (certId) REFERENCES CertTacking(certId)
+  FOREIGN KEY (personId) REFERENCES CertTacking (personId)
+  FOREIGN KEY (certId) REFERENCES CertTacking (certId)
 )
