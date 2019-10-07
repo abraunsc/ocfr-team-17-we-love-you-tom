8323e39328d4df0b6190fc1b894afea3c7496f43
kyao5452 <54466961+kyao5452@users.noreply.github.com>
2019-10-07 18:49:12 -0400

    chief dummy

---
+++ app/public/dummyChief.php
@@ -0,0 +1,15 @@
+[
+  {
+    "personId": "1",
+    "firstName": "Hillol ",
+    "lastName": "Bala",
+    "dob": "2012-09-01",
+    "address":"HH 4100 1309 E. 10th Street Bloomington IN 47405"
+    "email": "hbala@indiana.edu",
+    "gender": "Male",
+    "startDate": "2019-01-01",
+    "position": "Chief",
+    "radioNumber": "A-1",
+    "isActive": "0"
+  }
+]
