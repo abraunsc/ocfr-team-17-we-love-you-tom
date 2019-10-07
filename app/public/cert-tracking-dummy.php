7393aedcbd215f05d77fe041a56c91a3e0f06aee
abraunsc <abraunsc@iu.edu>
2019-10-07 18:56:28 -0400

    dummy file

---
+++ app/public/cert-tracking-dummy.php
@@ -0,0 +1,26 @@
+[
+{
+  'personID': 2,
+  'certID': 4,
+  'dateRenewed': '2018-3-12',
+  'expDate': '2019-4-22'
+},
+{
+  'personID': 6,
+  'certID': 1,
+  'dateRenewed': '2018-8-1',
+  'expDate': '2019-7-12'
+},
+{
+  'personID': 2,
+  'certID': 4,
+  'dateRenewed': '2015-12-25',
+  'expDate': '2019-2-20'
+},
+{
+  'personID': 8,
+  'certID': 5,
+  'dateRenewed': '2019-9-15',
+  'expDate': '2022-2-23'
+}
+]
