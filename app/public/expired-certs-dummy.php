7733135117a0d658b4ed2008bfa42a705e8c3e88
abraunsc <abraunsc@iu.edu>
2019-10-07 18:49:59 -0400

    dummy data php

---
+++ app/public/expired-certs-dummy.php
@@ -0,0 +1,20 @@
+[
+{
+  'personID': 2,
+  'certID': 4,
+  'dateRenewed': '2018-3-12',
+  'expDate': '2019-4-22'
+},
+{
+  'personID': 6,
+  'certID': 1,
+  'dateRenewed': '2018-8-1',
+  'expDate': '2019-7-12'
+},
+{
+  'personID': 2,
+  'certID': 4,
+  'dateRenewed': '2015-12-25',
+  'expDate': '2019-2-20'
+}
+]
