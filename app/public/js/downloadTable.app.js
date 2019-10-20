dd165d28bc7b351a2a5460489fe99d9c21e57582
abraunsc <abraunsc@iu.edu>
2019-10-19 22:23:56 -0400

    export to csv functionality and minor formatting

--- app/public/js/downloadTable.app.js
+++ app/public/js/downloadTable.app.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
    $('table').each(function () {
        var $table = $(this);
-
-       var $button = $("<button type='button'>");
+       var $button = $("<button type='button' class='btn btn-primary'>");
        $button.text("Export to spreadsheet");
        $button.insertAfter($table);
 
        $button.click(function () {
            var csv = $table.table2CSV({
-               delivery: 'value'
+               delivery: 'value',
            });
            window.location.href = 'data:text/csv;charset=UTF-8,' + encodeURIComponent(csv);
        });
